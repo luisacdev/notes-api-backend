f8d39daf5ae5c6e22c55d4d36c85d1c014b5e53c
Luis <luis.amacam@gmail.com>
2021-07-26 19:37:09 +0200

    Heroku PORT

--- index.js
+++ index.js
@@ -84,7 +84,8 @@ app.use((request, response) => {
   })
 })
 
-const PORT = process.env.PORT | 3001
+// Config PORT Heroku
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
